40684088a50a5d839e463d365d6661b438676df2
abdul <mateen.fastians@gmail.com>
2025-11-10 18:54:36 +0900

    [fix] vercel build for production

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
